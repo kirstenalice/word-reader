8cfd2527507e46897106c5e2e87ec356809ea6ad
Kirsten Meidlinger <kirstenalice426@gmail.com>
2020-06-10 01:35:50 -0400

    fuckkkk

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-var jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM();
-const { document } = (new JSDOM('')).window;
-global.document = document;
-
-var $ = jQuery = require('jquery')(window);
-
 const express = require("express");
 const bodyParser = require('body-parser');
 const say = require('say');
