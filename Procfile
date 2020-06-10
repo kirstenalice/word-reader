11ea383d8224440c576b7443582ac95ad26abe11
Kirsten Meidlinger <kirstenalice426@gmail.com>
2020-06-09 23:08:24 -0400

    removed routes folder

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: sudo apt-get install festival festvox-kallpc16k
 web: npm start
--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const say = require('say');
 const app = express();
 const configRoutes = require("./routes");
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
-configRoutes(app);
+app.get("/", async (req,res) => {
+  try{
+      say.speak(req.query.words);
+  } catch (err) {
+     console.log(err); 
+  }
+  res.sendFile("index.html", { root: "./public" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Speech Server");
